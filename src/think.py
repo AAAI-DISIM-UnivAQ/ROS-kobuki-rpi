f2046102bf80342dc1441fba0288905ba3f8a9eb
Giovanni De Gasperis <giovanni@giodegas.it>
2017-11-06 18:19:16 +0100

    Create think.py

---
+++ src/think.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+import time
+from kobuki_project.msg import Status
+
+'''
+  Here write your code to make a smart turtle bot that knows were to go
+'''
