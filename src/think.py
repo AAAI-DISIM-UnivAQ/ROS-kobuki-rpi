dcc39d84710ed1ad0018a1a115367b75de4b84f5
Giovanni De Gasperis <giovanni@giodegas.it>
2017-11-06 18:21:50 +0100

    Update think.py

--- src/think.py
+++ src/think.py
@@ -7,4 +7,5 @@ from kobuki_project.msg import Status
 
 '''
   Here write your code to make a smart turtle bot that knows were to go
+  receive from /kobuki_status and publish actions to /kobuki_velocity
 '''
